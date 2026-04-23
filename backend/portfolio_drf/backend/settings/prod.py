e60082d823aac06f2e959fc1a7934ede6311d093
saroj <skycse@gmail.com>
2026-04-24 05:25:46 +0545

    fix production settings (ALLOWED_HOSTS + CSRF)

--- backend/portfolio_drf/backend/settings/prod.py
+++ backend/portfolio_drf/backend/settings/prod.py
@@ -6,11 +6,12 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     host.strip()
-    for host in os.getenv("ALLOWED_HOSTS", "").split(",")
+    for host in os.getenv(
+        "ALLOWED_HOSTS",
+        "127.0.0.1,localhost,portfolio2026-buti.onrender.com"
+    ).split(",")
     if host.strip()
 ]
-
-
 
 DATABASES = {
     "default": dj_database_url.parse(
@@ -19,11 +20,19 @@ DATABASES = {
     )
 }
 
-
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://saroj01.com.np",
+    "https://portfolio2026-buti.onrender.com"
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://saroj01.com.np",
+    "https://portfolio2026-buti.onrender.com"
+]
