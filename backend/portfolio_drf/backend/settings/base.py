d20a52fa4cee0471196815143aec18717348f391
saroj <skycse@gmail.com>
2026-04-25 10:08:44 +0545

    settings updated

--- backend/portfolio_drf/backend/settings/base.py
+++ backend/portfolio_drf/backend/settings/base.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
